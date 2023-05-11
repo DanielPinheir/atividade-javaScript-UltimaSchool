c55053c483c7ae2e543bd9d28be516e37fbcf1bd
Daniel Pinheiro Machado <dyelzim@gmail.com>
2023-05-11 17:42:49 -0300

    update third challenge

--- terceiroDesafio/src/index.js
+++ terceiroDesafio/src/index.js
@@ -2,18 +2,22 @@ const arrayNumbers = [2, 9, 10.75, 14.74, 22.33];
 
 arrayNumbers.forEach((number) => {
   if (arrayNumbers.indexOf(number) === 0) {
-    console.log(Math.pow(number, 2));
+    console.log(`Quadrado de ${number}: ${Math.pow(number, 2)}`);
   }
   if (arrayNumbers.indexOf(number) === 1) {
-    console.log(Math.sqrt(number));
+    console.log(`Raiz quadrada de ${number}: ${Math.sqrt(number)}`);
   }
   if (arrayNumbers.indexOf(number) === 2) {
-    console.log(Math.trunc(number));
+    console.log(`Parte inteira de ${number}: ${Math.trunc(number)}`);
   }
   if (arrayNumbers.indexOf(number) === 3) {
-    console.log(Math.floor(number));
+    console.log(
+      `Arredondando para baixo o número ${number}: ${Math.floor(number)}`
+    );
   }
   if (arrayNumbers.indexOf(number) === 4) {
-    console.log(Math.ceil(number));
+    console.log(
+      `Arredondando para cima o número ${number}: ${Math.ceil(number)}`
+    );
   }
 });
